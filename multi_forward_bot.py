831d35d05339ea2ff0a3ab716a691a2a0fe29669
DeadManStudy <119425801+DeadManStudy@users.noreply.github.com>
2026-01-25 17:25:15 +0900

    Update multi_forward_bot.py

--- multi_forward_bot.py
+++ multi_forward_bot.py
@@ -1,27 +1,24 @@
 import os
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 
 TARGET_CHATS = [
     -5258812606,
 ]
 
-def forward_all(update, context):
+async def forward_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.effective_chat.type != "private":
         return
 
     for chat_id in TARGET_CHATS:
-        context.bot.copy_message(
+        await context.bot.copy_message(
             chat_id=chat_id,
             from_chat_id=update.effective_chat.id,
             message_id=update.message.message_id
         )
 
-updater = Updater(BOT_TOKEN, use_context=True)
-dp = updater.dispatcher
-
-dp.add_handler(MessageHandler(Filters.all, forward_all))
-
-updater.start_polling()
-updater.idle()
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, forward_all))
+app.run_polling()
